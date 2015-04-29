53bda91fd456076db1e6ce8508725985d86e91fd
Alen Pelin <alienlab.com@gmail.com>
2015-04-29 19:25:00 +1000

    Fixed #1 (Implement 'Purge from caches' button)

---
+++ Pipelines/RemoveItemFromCaches/RemoveItemFromAccessResultCache.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.Pipelines.RemoveItemFromCaches
+{
+  using Sitecore.Caching;
+  using Sitecore.Diagnostics;
+
+  public class RemoveItemFromAccessResultCache
+  {
+    [UsedImplicitly]
+    public void Process([NotNull] RemoveItemFromCachePipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      var item = args.Item;
+
+      var cache = CacheManager.GetAccessResultCache();
+      Assert.IsNotNull(cache, "cache");
+
+      var id = item.ID;
+      Assert.IsNotNull(id, "id");
+
+      cache.RemoveKeysContaining(id.ToString());
+    }
+  }
+}
---
+++ Shell/Framework/Commands/PurgeFromCaches.cs
@@ -0,0 +1,26 @@
+﻿namespace Sitecore.Support.Shell.Framework.Commands
+{
+  using System.Linq;
+  using Sitecore.Diagnostics;
+  using Sitecore.Pipelines;
+  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Support.Pipelines.RemoveItemFromCaches;
+
+  [UsedImplicitly]
+  public class PurgeFromCaches : Command
+  {
+    public override void Execute([NotNull] CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+
+      var items = context.Items;
+      Assert.IsNotNull(items, "items");
+
+      var item = items.FirstOrDefault();
+      if (item != null)
+      {
+        Pipeline.Start("removeItemFromCache", new RemoveItemFromCachePipelineArgs(item));
+      }
+    }
+  }
+}
