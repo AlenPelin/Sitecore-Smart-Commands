b74f5be9211ca2ba27c385c8761736b3b1d47b52
Ahmed Okour <ahmed.okour@americaneagle.com>
2017-11-05 18:16:41 -0600

    ReferenceReplacementJon is obsolete, Replacing it with running pipeline "replaceItemReferences", also creating helper method to update links when creating items of branch templates

---
+++ src/Sitecore.SmartCommands/Helper.cs
@@ -0,0 +1,212 @@
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.SmartCommands
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Replace item references (Datasource links) with new ones that are descendant of it
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="copy"></param>
+        /// <param name="deep"></param>
+        /// <param name="isAsync"></param>
+        public static void ReplaceItemReferences(Item source, Item copy, bool deep, bool isAsync)
+        {
+            Sitecore.Pipelines.CorePipeline.Run("replaceItemReferences", new Sitecore.Pipelines.ReplaceItemReferences.ReplaceItemReferencesArgs()
+            {
+                SourceItem = source,
+                CopyItem = copy,
+                Deep = deep,
+                Async = isAsync
+            });
+        }
+
+        /// <summary>
+        /// Utility method for relinking branch datasources
+        /// </summary>
+        public static void RelinkDatasourcesInBranchInstance(Item item, bool descendants = false)
+        {
+            RelinkDatasourcesForItemInBranchInstance(item, item);
+
+            if (!descendants)
+            {
+                return;
+            }
+
+            foreach (var descendant in item.Axes.GetDescendants())
+            {
+                RelinkDatasourcesForItemInBranchInstance(descendant, item);
+            }
+        }
+
+        /// <summary>
+        /// Utility method for relinking datasources for an item within a branch instance
+        /// </summary>
+        /// <remarks>
+        /// Adapted from original code, written by Kam Figy: 
+        /// https://github.com/kamsar/BranchPresets/blob/master/BranchPresets/AddFromBranchPreset.cs
+        /// </remarks>
+        public static void RelinkDatasourcesForItemInBranchInstance(Item item, Item instanceRoot)
+        {
+            Action<RenderingDefinition> relinkRenderingDatasource =
+                rendering =>
+                    RelinkRenderingDatasourceForItemInBranch(item, instanceRoot, rendering);
+
+            ApplyActionToAllRenderings(item, relinkRenderingDatasource);
+        }
+
+
+        /// <summary>
+        /// Utility method for relinking the datasource for the supplied rendering on an item in the branch instance
+        /// </summary>
+        /// <remarks>
+        /// Adapted from original code, written by Kam Figy: 
+        /// https://github.com/kamsar/BranchPresets/blob/master/BranchPresets/AddFromBranchPreset.cs
+        /// </remarks>
+        /// <param name="item">Item that contains the rendering</param>
+        /// <param name="instanceRoot">Root item of the branch instance</param>
+        /// <param name="rendering">Rendering to be relinked</param>
+        public static void RelinkRenderingDatasourceForItemInBranch(Item item, Item instanceRoot, RenderingDefinition rendering)
+        {
+            var branchBasePath = item.Branch.InnerItem.Paths.FullPath;
+
+            if (string.IsNullOrWhiteSpace(rendering.Datasource))
+            {
+                return;
+            }
+
+            var database = item.Database;
+
+            // note: queries and multiple item datasources are not supported
+            var renderingTargetItem = database.GetItem(rendering.Datasource);
+
+            Assert.IsNotNull(
+                renderingTargetItem,
+                String.Format("Error while expanding branch template rendering datasources: data source {0} was not resolvable.", rendering.Datasource)
+                    );
+
+            // if there was no valid target item OR the target item is not a child of the branch template we skip out
+            if (renderingTargetItem == null ||
+                !renderingTargetItem.Paths.FullPath.StartsWith(branchBasePath, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            // get the path relative to the branch item
+            var relativeRenderingPath =
+                renderingTargetItem.Paths.FullPath.Substring(branchBasePath.Length);
+
+            // replace $name Sitecore tokens in path
+            relativeRenderingPath = relativeRenderingPath
+                .Replace("$name", instanceRoot.Name);
+
+            var newTargetPath = instanceRoot.Parent.Paths.FullPath + relativeRenderingPath;
+            var newTargetItem = database.GetItem(newTargetPath);
+
+            // if the target item was a valid under branch item, but the same relative path does not exist under the branch instance
+            // we set the datasource to something invalid to avoid any potential unintentional edits of a shared data source item
+            rendering.Datasource = newTargetItem?.ID.ToString() ?? "INVALID_BRANCH_SUBITEM_ID";
+        }
+
+        /// <summary>
+        ///     Invokes Action on all Renderings on item
+        /// </summary>
+        public static void ApplyActionToAllRenderings(Item item, Action<RenderingDefinition> action)
+        {
+            ApplyActionToAllSharedRenderings(item, action);
+            ApplyActionToAllFinalRenderings(item, action);
+        }
+
+        /// <summary>
+        ///     Invokes Action on all Shared Renderings on item
+        /// </summary>
+        public static void ApplyActionToAllSharedRenderings(Item item, Action<RenderingDefinition> action)
+        {
+            ApplyActionToLayoutField(item, FieldIDs.LayoutField, action);
+        }
+
+        /// <summary>
+        ///     Invokes Action on all Final Renderings on item
+        /// </summary>
+        public static void ApplyActionToAllFinalRenderings(Item item, Action<RenderingDefinition> action)
+        {
+            ApplyActionToLayoutField(item, FieldIDs.FinalLayoutField, action);
+        }
+
+        /// <summary>
+        ///     Invokes Action on all Final Renderings on item
+        /// </summary>
+        private static void ApplyActionToLayoutField(Item item, ID fieldId, Action<RenderingDefinition> action)
+        {
+            var currentLayoutXml = LayoutField.GetFieldValue(item.Fields[fieldId]);
+            if (string.IsNullOrEmpty(currentLayoutXml))
+            {
+                return;
+            }
+
+            var newXml = ApplyActionToLayoutXml(currentLayoutXml, action);
+            if (newXml == null)
+            {
+                return;
+            }
+
+            using (new SecurityDisabler())
+            {
+                using (new EditContext(item))
+                {
+                    // NOTE: when dealing with layouts its important to get and set the field value with LayoutField.Get/SetFieldValue()
+                    // if you fail to do this you will not process layout deltas correctly and may instead override all fields (breaking full inheritance), 
+                    // or attempt to get the layout definition for a delta value, which will result in your wiping the layout details when they get saved.
+                    LayoutField.SetFieldValue(item.Fields[fieldId], newXml);
+                }
+            }
+        }
+
+        private static string ApplyActionToLayoutXml(string xml, Action<RenderingDefinition> action)
+        {
+            var layout = LayoutDefinition.Parse(xml);
+
+            // normalize the output in case of any minor XML differences (spaces, etc)
+            xml = layout.ToXml();
+
+            // loop over devices in the rendering
+            for (var deviceIndex = layout.Devices.Count - 1; deviceIndex >= 0; deviceIndex--)
+            {
+                var device = layout.Devices[deviceIndex] as DeviceDefinition;
+                if (device == null)
+                {
+                    continue;
+                }
+
+                // loop over renderings within the device
+                for (var renderingIndex = device.Renderings.Count - 1; renderingIndex >= 0; renderingIndex--)
+                {
+                    var rendering = device.Renderings[renderingIndex] as RenderingDefinition;
+                    if (rendering == null)
+                    {
+                        continue;
+                    }
+
+                    // run the action on the rendering
+                    action(rendering);
+                }
+            }
+
+            var layoutXml = layout.ToXml();
+
+            // save a modified layout value if necessary
+            return layoutXml != xml ? layoutXml : null;
+        }
+    }
+}
--- src/Sitecore.SmartCommands/Pipelines/CopyItems/UpdateLinks.cs
+++ src/Sitecore.SmartCommands/Pipelines/CopyItems/UpdateLinks.cs
@@ -1,81 +1,74 @@
 ﻿namespace Sitecore.SmartCommands.Pipelines.CopyItems
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using Sitecore.Configuration;
-  using Sitecore.Data.Items;
-  using Sitecore.Diagnostics;
-  using Sitecore.Shell.Framework.Pipelines;
-  using Sitecore.Jobs;
-  using Sitecore.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Sitecore.Configuration;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Shell.Framework.Pipelines;
+    using Sitecore.Jobs;
+    using Sitecore.Text;
 
-  public sealed class UpdateLinks
-  {
-    private readonly bool isAsync;
-
-    public UpdateLinks()
+    public sealed class UpdateLinks
     {
-      this.isAsync = false;
-    }
+        private readonly bool isAsync;
 
-    public UpdateLinks([NotNull] string async)
-    {
-      Assert.ArgumentNotNull(async, "async");
-
-      this.isAsync = string.Equals(async, "true", StringComparison.OrdinalIgnoreCase);
-    }
-
-    [UsedImplicitly]
-    public void Process([NotNull] CopyItemsArgs args)
-    {
-      Assert.ArgumentNotNull(args, "args");
-
-      var parameters = args.Parameters;
-      Assert.IsNotNull(parameters, "parameters");
-
-      if (!string.Equals(parameters["mode"], "smart", StringComparison.OrdinalIgnoreCase))
-      {
-        return;
-      }
-      
-      var copies = args.Copies;
-      Assert.IsNotNull(copies, "copies");
-
-      var sources = GetItems(args).ToArray();
-      for (var i = 0; i < copies.Length; i++)
-      {
-        var copy = copies[i];
-        var source = sources[i];
-        if (this.isAsync)
+        public UpdateLinks()
         {
-          ReferenceReplacementJob.StartAsync(source, copy);
+            this.isAsync = false;
         }
-        else
+
+        public UpdateLinks([NotNull] string async)
         {
-          ReferenceReplacementJob.Start(source, copy);
+            Assert.ArgumentNotNull(async, "async");
+
+            this.isAsync = string.Equals(async, "true", StringComparison.OrdinalIgnoreCase);
         }
-      }
 
-      // if mode is smart then this should be the last processor
-      args.AbortPipeline();
-    }
-
-    private static IEnumerable<Item> GetItems(CopyItemsArgs args)
-    {
-      var databaseName = args.Parameters["database"];
-      var database = Factory.GetDatabase(databaseName);
-      Assert.IsNotNull(database, "database");
-
-      var listString = new ListString(args.Parameters["items"], '|');
-      foreach (var idString in listString)
-      {
-        var item = database.GetItem(idString);
-        if (item != null)
+        [UsedImplicitly]
+        public void Process([NotNull] CopyItemsArgs args)
         {
-          yield return item;
+            Assert.ArgumentNotNull(args, "args");
+
+            var parameters = args.Parameters;
+            Assert.IsNotNull(parameters, "parameters");
+
+            if (!string.Equals(parameters["mode"], "smart", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            var copies = args.Copies;
+            Assert.IsNotNull(copies, "copies");
+
+            var sources = GetItems(args).ToArray();
+            for (var i = 0; i < copies.Length; i++)
+            {
+                var copy = copies[i];
+                var source = sources[i];
+                Helper.ReplaceItemReferences(source, copy, true, isAsync);
+            }
+
+            // if mode is smart then this should be the last processor
+            args.AbortPipeline();
         }
-      }
+
+        private static IEnumerable<Item> GetItems(CopyItemsArgs args)
+        {
+            var databaseName = args.Parameters["database"];
+            var database = Factory.GetDatabase(databaseName);
+            Assert.IsNotNull(database, "database");
+
+            var listString = new ListString(args.Parameters["items"], '|');
+            foreach (var idString in listString)
+            {
+                var item = database.GetItem(idString);
+                if (item != null)
+                {
+                    yield return item;
+                }
+            }
+        }
     }
-  }
 }
